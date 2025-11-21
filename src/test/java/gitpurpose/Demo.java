c59ea5227b7f90d31b1ed8c936513af87d3082e1
kavya19980101 <kavyastestings@gmail.com>
2025-11-21 15:52:45 +0530

    new changes of cart module

--- src/test/java/gitpurpose/Demo.java
+++ src/test/java/gitpurpose/Demo.java
@@ -5,6 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("Hi, this is product module update");
 		System.out.println("Hi, this is product module updated1");
+		System.out.println("Hi, this is product module updated2");
 	}
 	
 }
